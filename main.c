0214fe6be088b7a3fdf43401ad82fc990dcf6ab7
chris <er1293545211@live.com>
2023-08-11 09:25:24 +0800

    random

--- main.c
+++ main.c
@@ -8,18 +8,24 @@
 //全局未初始化区
 char *p1;
 
+int USE_MEMORY = 0;
+int rand_sleep = 0;
+int rand_use_memory = 0;
+
 // TODO 占用1G内存
-int main() {
-    printf("%d\n", (1 << 30));
-    printf("char: %d\n", sizeof(char));
-    printf("int: %d\n", sizeof(int));
-    p1 = (char *) malloc(1 << 30);
+void main() {
+//    printf("%d\n", (1 << 30));
+//    printf("char: %d\n", sizeof(char));
+//    printf("int: %d\n", sizeof(int));
 
-    printf("memset\n");
-
-    // 向内存空间内填充数据
-    memset(p1, 0xFF, 1 << 30);
-    sleep(30);
+    // 生成 m ~ n 的随机数
+    // int rand_number = rand() % (n - m + 1) + m;
+    /*while (1) {
+        // 生成 90 ~ 100 的随机数，包含 90 和 100
+        int rand_number = rand() % 11 + 90;
+        printf("random number: %d\n", rand_number);
+        sleep(1);
+    }*/
 
     // TODO
     // 打印占用的内存空间
@@ -27,11 +33,35 @@ int main() {
     // https://blog.csdn.net/m0_61025131/article/details/119987809
     // https://blog.csdn.net/qq_45656248/article/details/116395312
 
-    printf("free\n");
-    // 释放内存
-    free(p1);
-    sleep(30);
-    return 0;
+    while (1) {
+        printf("USE_MEMORY: %d\n", USE_MEMORY);
+        if (USE_MEMORY) {
+            printf("use memory\n");
+
+            // 占用内存
+            // 分配空间
+            p1 = (char *) malloc(1 << 30);
+            // 向内存空间内填充数据
+            memset(p1, 0xFF, 1 << 30);
+            printf("memset\n");
+
+            // 获取随机数，20 ~ 30
+            rand_sleep = rand() % 11 + 20;
+            printf("rand_sleep: %d\n", rand_sleep);
+
+            sleep(rand_sleep);
+        } else {
+            // 释放内存
+            printf("release memory\n");
+            // 释放内存
+            free(p1);
+
+            // 获取随机数，20 ~ 30
+            rand_use_memory = rand() % 11 + 20;
+            printf("rand_use_memory: %d\n", rand_use_memory);
+            sleep(rand_use_memory);
+        }
+    }
 }
 
 /*#include <stdio.h>
